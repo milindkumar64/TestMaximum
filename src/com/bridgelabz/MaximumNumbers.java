71a386bf54ccba03568e06ba56c8f329048a61e7
milindkumar64 <milindgupta@gmail.com>
2023-04-06 01:17:39 +0530

    Add: Largest Float Number among Three Float Numbers

--- src/com/bridgelabz/MaximumNumbers.java
+++ src/com/bridgelabz/MaximumNumbers.java
@@ -2,8 +2,8 @@ package com.bridgelabz;
 
 class MaximumNumber {
 
-	public static int maximum(Integer a, Integer b, Integer c) {
-		int max = a;
+	public static float maximum(Float a, Float b, Float c) {
+		float max = a;
 		if (b.compareTo(max) > 0)
 			max = b;
 		if (c.compareTo(max) > 0)
@@ -14,9 +14,9 @@ class MaximumNumber {
 
 	public static void main(String[] args) {
 
-		Integer a = new Integer(20);
-		Integer b = new Integer(30);
-		Integer c = new Integer(40);
+		Float a = new Float(20.3);
+		Float b = new Float(30.5);
+		Float c = new Float(40.2);
 
 		System.out.println(maximum(a, b, c));
 
