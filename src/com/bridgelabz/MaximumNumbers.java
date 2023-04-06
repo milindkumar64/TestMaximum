0da51638643f12c8eb7ed1e2c83c95d8433e64cd
milindkumar64 <milindgupta@gmail.com>
2023-04-06 13:16:32 +0530

    Add: maximum in three String

--- src/com/bridgelabz/MaximumNumbers.java
+++ src/com/bridgelabz/MaximumNumbers.java
@@ -2,8 +2,8 @@ package com.bridgelabz;
 
 class MaximumNumber {
 
-	public static float maximum(Float a, Float b, Float c) {
-		float max = a;
+	public static String maximum(String a, String b, String c) {
+		String max = a;
 		if (b.compareTo(max) > 0)
 			max = b;
 		if (c.compareTo(max) > 0)
@@ -14,9 +14,9 @@ class MaximumNumber {
 
 	public static void main(String[] args) {
 
-		Float a = new Float(20.3);
-		Float b = new Float(30.5);
-		Float c = new Float(40.2);
+		String a = new String("Milind");
+		String b = new String("Saras");
+		String c = new String("Om");
 
 		System.out.println(maximum(a, b, c));
 	}
