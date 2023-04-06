b82ad677df450f4ec0cdfc799843babf06b23661
milindkumar64 <milindgupta@gmail.com>
2023-04-06 14:39:36 +0530

    Add: extending max Method with internally calling print method

--- src/com/bridgelabz/MaximumNumbers.java
+++ src/com/bridgelabz/MaximumNumbers.java
@@ -6,35 +6,37 @@ class MaximumNumber<M extends Comparable<M>> {
 	private M b;
 	private M c;
 	private M d;
-	
-	public MaximumNumber(M a, M b, M c,M d) {
+
+	public MaximumNumber(M a, M b, M c, M d) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
 		this.d = d;
-
 	}
 
-	public <M extends Comparable<M>> M testMaximum(M a, M b, M c, M d) {
+	public <M extends Comparable<M>> void testMaximum(M a, M b, M c, M d) {
 		M max = a;
 		if (b.compareTo(max) > 0)
 			max = b;
 		if (c.compareTo(max) > 0)
 			max = c;
-		if(d.compareTo(max) > 0)
+		if (d.compareTo(max) > 0)
 			max = d;
+		printMax(max);
+	}
 
-		return max;
+	public static <M> void printMax(M max) {
+		System.out.println("maximum is : " + max);
 	}
 
 	public static void main(String[] args) {
 
-		MaximumNumber<Integer> obj1 = new MaximumNumber<Integer>(10, 20, 30,40);
-		MaximumNumber<Float> obj2 = new MaximumNumber<Float>(10.3f, 20.3f, 30.4f,40.8f);
-		MaximumNumber<String> obj3 = new MaximumNumber<String>("Apple", "Peach", "Banana","Orange");
+		MaximumNumber<Integer> obj1 = new MaximumNumber<Integer>(10, 20, 30, 40);
+		MaximumNumber<Float> obj2 = new MaximumNumber<Float>(10.3f, 20.3f, 30.4f, 40.8f);
+		MaximumNumber<String> obj3 = new MaximumNumber<String>("Apple", "Peach", "Banana", "Orange");
 
-		System.out.println(obj1.testMaximum(obj1.a, obj1.b, obj1.c,obj1.d));
-		System.out.println(obj2.testMaximum(obj2.a, obj2.b, obj2.c,obj2.d));
-		System.out.println(obj3.testMaximum(obj3.a, obj3.b, obj3.c,obj3.d));
+		obj1.testMaximum(obj1.a, obj1.b, obj1.c, obj1.d);
+		obj2.testMaximum(obj2.a, obj2.b, obj2.c, obj2.d);
+		obj3.testMaximum(obj3.a, obj3.b, obj3.c, obj3.d);
 	}
 }
