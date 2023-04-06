894edc0a038fb452122e673e17eed4a4e7a8f7b3
milindkumar64 <milindgupta@gmail.com>
2023-04-06 14:00:13 +0530

    Refactor2: Generic class with three varable of Generic type

--- src/com/bridgelabz/MaximumNumbers.java
+++ src/com/bridgelabz/MaximumNumbers.java
@@ -1,25 +1,37 @@
 package com.bridgelabz;
 
-public class MaximumNumbers {
+class MaximumNumber<M extends Comparable<M>> {
 
-	public static <M extends Comparable<M>> M maximum(M x, M y, M z) {
-		M max = x;
-		if (y.compareTo(max) > 0) {
-			max = y;
-		}
-		if (z.compareTo(max) > 0)
-			max = z;
+	private M a;
+	private M b;
+	private M c;
 
+	public MaximumNumber(M a, M b, M c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+
+	}
+
+	public <M extends Comparable<M>> M testMaximum(M a, M b, M c) {
+		M max = a;
+		if (b.compareTo(max) > 0)
+			max = b;
+		if (c.compareTo(max) > 0)
+			max = c;
 
 		return max;
 	}
 
 	public static void main(String[] args) {
 
-		System.out.println("Largest Integer : "+maximum(20,30,40));
-		System.out.println("Largest Float : "+maximum(20.3,30.5,40.6));
-		System.out.println("Largest String in lexicographically order : "+maximum("Apple", "Peach", "Banana"));
+		MaximumNumber<Integer> obj1 = new MaximumNumber<Integer>(10, 20, 30);
+		MaximumNumber<Float> obj2 = new MaximumNumber<Float>(10.3f, 20.3f, 30.4f);
+		MaximumNumber<String> obj3 = new MaximumNumber<String>("Apple", "Peach", "Banana");
 
+		System.out.println(obj1.testMaximum(obj1.a, obj1.b, obj1.c));
+		System.out.println(obj2.testMaximum(obj2.a, obj2.b, obj2.c));
+		System.out.println(obj3.testMaximum(obj3.a, obj3.b, obj3.c));
 	}
 
 }
