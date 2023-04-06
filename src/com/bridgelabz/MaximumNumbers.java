8bb64815c7cd8c913a9759b699041cf58350bf84
milindkumar64 <milindgupta@gmail.com>
2023-04-06 13:36:51 +0530

    Refactor1: all the three using Generic Method

--- src/com/bridgelabz/MaximumNumbers.java
+++ src/com/bridgelabz/MaximumNumbers.java
@@ -1,22 +1,25 @@
 package com.bridgelabz;
 
-class MaximumNumber {
+public class MaximumNumbers {
 
-	public static String maximum(String a, String b, String c) {
-		String max = a;
-		if (b.compareTo(max) > 0)
-			max = b;
-		if (c.compareTo(max) > 0)
-			max = c;
+	public static <M extends Comparable<M>> M maximum(M x, M y, M z) {
+		M max = x;
+		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (z.compareTo(max) > 0)
+			max = z;
+
 
 		return max;
 	}
+
 	public static void main(String[] args) {
 
-		String a = new String("Milind");
-		String b = new String("Saras");
-		String c = new String("Om");
+		System.out.println("Largest Integer : "+maximum(20,30,40));
+		System.out.println("Largest Float : "+maximum(20.3,30.5,40.6));
+		System.out.println("Largest String in lexicographically order : "+maximum("Apple", "Peach", "Banana"));
 
-		System.out.println(maximum(a, b, c));
 	}
+
 }
